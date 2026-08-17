9abb1be374a5dc27e0a337d8068f2b9ce876de78
FABRIKA <hanymaged777@gmail.com>
2026-08-17 18:32:24 +0300

    initial commit

---
+++ app/src/main/java/com/example/contactbook/ContactsOpenHelper.java
@@ -0,0 +1,61 @@
+package com.example.contactbook;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.ContentValues;
+import android.database.Cursor;
+public class ContactsOpenHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "contacts.db";
+    private static final int DATABASE_VERSION = 1;
+
+
+    // table name
+    public static final String TABLE_CONTACTS = "contacts";
+
+    // columns names
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_PHONE = "phone";
+    public static final String COLUMN_EMAIL = "email";
+    public static final String COLUMN_CATEGORY = "category";
+    public static final String COLUMN_PHONE_TYPE = "phone_type";
+
+    public ContactsOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_CONTACTS + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_NAME + " TEXT,"
+                + COLUMN_PHONE + " TEXT,"
+                + COLUMN_EMAIL + " TEXT,"
+                + COLUMN_CATEGORY + " TEXT,"
+                + COLUMN_PHONE_TYPE + " TEXT"
+                + ")";
+        db.execSQL(CREATE_CONTACTS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
+        onCreate(db);
+
+    }
+    public void addContact(String name, String phone, String email, String category, String phoneType) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME, name);
+        values.put(COLUMN_PHONE, phone);
+        values.put(COLUMN_EMAIL, email);
+        values.put(COLUMN_CATEGORY, category);
+        values.put(COLUMN_PHONE_TYPE, phoneType);
+
+        db.insert(TABLE_CONTACTS, null, values);
+        db.close();
+    }
+}
